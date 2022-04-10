d153b7c0cb554522d6bacbc6a3dc15082e410eb2
MarcoV <marco.verzeni@mail.polimi.it>
2022-04-10 21:55:43 +0200

    add charts and date filter

---
+++ src/utils/computeReliabilityScore.ts
@@ -0,0 +1,14 @@
+export const maxDelayForAGoodJourney = 1000 * 60 * 5; //in milliseconds
+
+const computeReliabilityScore = (journeys: any) => {
+  const NumOfGoodJourneys = journeys.filter((journey: any) => {
+    if (journey.isCanceled === true) {
+      return false;
+    }
+    return journey.delay <= maxDelayForAGoodJourney;
+  }).length;
+  console.log(NumOfGoodJourneys, journeys.length);
+  return NumOfGoodJourneys / journeys.length;
+};
+
+export default computeReliabilityScore;
